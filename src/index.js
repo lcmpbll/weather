b864a1c17564eca9273c1aa00ce27156b4451db8
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-28 11:31:52 -0700

    Separate service logic.
    Co-authored-by: Ryan DeFea <ryan.defea@gmail.com>
    Co-authored-by: PJ Perry <pjperry28@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,28 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './Weather-service.js'
+
+function clearFields() {
+  $('#location').val("");
+  $('.showErrors').val("");
+  $('.showHumidity').val("");
+  $('.showTemp').val("");
+}
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
-
-    let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-    }
+    let city = $('#location').val();
+    clearFields();
+    let promise = WeatherService.getWeather(city);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Farenheit is ${body.main.temp} degrees.`);
+      $('.showErrors').text("");
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
   });
 });
